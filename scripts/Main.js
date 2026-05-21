46c398f92b363cf6b417f5f3e978bdcad4eda8f4
Naga185 <166745517+Naga185@users.noreply.github.com>
2026-05-21 23:13:40 +0530

    Refactor Main.js to use widget diagnostics

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,41 +1,25 @@
-define('scripts/Main', ['DS/WAFData/WAFData'], function (WAFData) {
+define('scripts/Main', [], function () {
     'use strict';
 
     var MainWidget = {
         init: function () {
-            // 1. Grab the platform instance URL out of the window location parameters
-            var urlParams = new URLSearchParams(window.location.search);
-            var platformUrl = urlParams.get('x3dPlatformId') || 'https://r1132100379178-ap2-ifwe.3dexperience.3ds.com';
-            
-            // 2. Build the official passport endpoint string
-            var passportUrl = platformUrl.replace(/\/$/, '') + '/3DPassport/users/current';
+            var diagnosticInfo = '';
 
-            // 3. Request your profile data securely using the platform's trusted authentication tunnel
-            WAFData.authenticatedRequest(passportUrl, {
-                method: 'GET',
-                type: 'json',
-                onComplete: function (response) {
-                    var displayName = 'User';
-                    if (response) {
-                        if (response.preferredLanguage) { // Verifies it's a valid profile object
-                            var firstName = response.firstName || '';
-                            var lastName = response.lastName || '';
-                            displayName = (firstName + ' ' + lastName).trim() || response.name || 'User';
-                        }
-                    }
-                    MainWidget.render(displayName);
-                },
-                onFailure: function () {
-                    // Fallback string if the security policy is restricted
-                    MainWidget.render('User');
-                }
-            });
-        },
+            if (typeof widget !== 'undefined') {
+                // Fetch basic platform identifiers that don't violate security policies
+                var platformId = widget.getValue('x3dPlatformId') || 'Not Found';
+                var lang = widget.lang || 'Not Detected';
+                
+                diagnosticInfo += '<p><strong>Platform ID:</strong> ' + platformId + '</p>';
+                diagnosticInfo += '<p><strong>Language:</strong> ' + lang + '</p>';
+                diagnosticInfo += '<p><strong>Widget ID:</strong> ' + (widget.id || 'N/A') + '</p>';
+            } else {
+                diagnosticInfo = '<p style="color:red;">Widget object is not accessible!</p>';
+            }
 
-        render: function (name) {
             var contentDiv = document.getElementById('widget-content');
             if (contentDiv) {
-                contentDiv.innerHTML = '<h1>Hello World, ' + name + '!</h1>';
+                contentDiv.innerHTML = '<h1>Diagnostic Test Success!</h1>' + diagnosticInfo;
             }
         }
     };
